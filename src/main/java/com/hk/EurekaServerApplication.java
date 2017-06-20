8985709afec37fdaeae3ccfe8836822cfa6dc6f9
minghui <459245607@qq.com>
2017-06-20 14:26:23 +0800

    Initial commit

---
+++ src/main/java/com/hk/EurekaServerApplication.java
@@ -0,0 +1,14 @@
+package com.hk;
+
+		import org.springframework.boot.SpringApplication;
+		import org.springframework.boot.autoconfigure.SpringBootApplication;
+		import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer //开启eureka注册服务
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
